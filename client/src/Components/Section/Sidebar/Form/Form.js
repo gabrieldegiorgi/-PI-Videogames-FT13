e47d88a613a0a0f86c36c6ec33d6bfc3c2401a79
Gabriel <degiorgi.gabriel@gmail.com>
2021-06-23 21:26:35 -0300

    El juego creado aparece en el front

--- client/src/Components/Section/Sidebar/Form/Form.js
+++ client/src/Components/Section/Sidebar/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { validate } from "../../../../utils";
 import "./Form.css";
 import { RiCloseCircleFill } from "react-icons/ri";
@@ -12,18 +12,57 @@ const genres = [
   { id: 3, name: "Shooter" },
   { id: 4, name: "RPG" },
   { id: 5, name: "Strategy" },
-  { id: 6, name: "Casual" }
+  { id: 6, name: "Casual" },
+];
+
+const platforms = [
+  { id: 1, name: "PC" },
+  { id: 2, name: "Playstation 2" },
+  { id: 3, name: "Playstation 3" },
+  { id: 4, name: "Playstation 4" },
+  { id: 5, name: "xBox 360" },
+  { id: 6, name: "Nintendo" },
 ];
 
 function Form({ showModal, setShowModal }) {
   const [errors, setErrors] = useState({});
+  const [genresSelected, setGenresSelected] = useState([]);
+  const [platformsSelected, setPlatformsSelected] = useState([]);
 
   const [input, setInput] = useState({
     name: "",
     description: "",
     date: "",
     rating: "",
+    genres: [],
   });
+
+  //POR CADA VEZ QUE EL USUARIO SELECCIONA,
+  //AL ESTADO GENRES LE TENGO QUE DECIR
+
+  useEffect(() => {
+    var array = [];
+
+    array = genresSelected.map((g) => g.name);
+
+    console.log(input.genres)
+
+    setInput({
+      ...input,
+      genres: array,
+    });
+  }, [genresSelected]);
+
+  useEffect(() => {
+    var array = [];
+
+    array = platformsSelected.map((p) => p.name);
+
+    setInput({
+      ...input,
+      platforms: array,
+    });
+  }, [platformsSelected]);
 
   const dispatch = useDispatch();
   const modalRef = useRef(); //Hook de react que me permite tener una referencia
@@ -37,6 +76,8 @@ function Form({ showModal, setShowModal }) {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    console.log("Esto es input", input);
     dispatch(createVideogame(input));
   };
 
@@ -111,26 +152,22 @@ function Form({ showModal, setShowModal }) {
                 />
                 {errors.rating && <p className="danger">{errors.rating}</p>}
               </div>
-              <div className="form-element">
-                <select>
-                  <Dropdown items={genres} />
-                  {/*   <option value="Accion">Accion</option>
-                  <option value="Aventura">Aventura</option>
-                  <option value="RPG">RPG</option>
-                  <option value="Estrategia">Estrategia</option>
-                  <option value="Shooter">Shooter</option>
-                  <option value="Causal">Casual</option> */}
-                </select>
-              </div>
-              <div className="form-element">
-                {/* <select>
-                  <option value="Accion">Playstation</option>
-                  <option value="Aventura">Computadora</option>
-                  <option value="RPG">Nintendo</option>
-                  <option value="Estrategia">Otra</option>
-                  <option value="Shooter">Otra 2</option>
-                  <option value="Causal">Otra 3</option>
-                </select> */}
+
+              <div className="row-element">
+                <Dropdown
+                  title="genres"
+                  items={genres}
+                  multiselect
+                  selection={genresSelected}
+                  setSelection={setGenresSelected}
+                />
+                <Dropdown
+                  title="platforms"
+                  items={platforms}
+                  multiselect
+                  selection={platformsSelected}
+                  setSelection={setPlatformsSelected}
+                />
               </div>
 
               <button
